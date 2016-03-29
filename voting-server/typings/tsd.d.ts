309b57abe046da8b3b2ac6bfd0942556f0167f84
cdroulers <droulers.christian@gmail.com>
2016-03-29 17:14:41 -0400

    Redux?

--- voting-server/typings/tsd.d.ts
+++ voting-server/typings/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="assertion-error/assertion-error.d.ts" />
 /// <reference path="chai/chai.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="redux/redux.d.ts" />
